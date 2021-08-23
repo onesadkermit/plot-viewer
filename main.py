ca23ae3bffa7ad04c895519e8df4f8f9512208ec
Ilya Mirolubov <ilya_mirolubov@mail.ru>
2021-08-23 17:55:46 +0300

    Initial commit
    some random code, tkinter+matplotlib

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+main_window = Tk()
+main_window.mainloop()
+
+print("hello")
